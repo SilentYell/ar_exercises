d9d072dcae6429afdaa748ceb14e5754062798ca
Nik Hixson <nikilobe@gmail.com>
2025-03-14 13:05:07 -0700

    Add employees to store 1 and store 2 with specified hourly rates

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,10 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "John", last_name: "Doe", hourly_rate: 50)
+@store2.employees.create(first_name: "Jane", last_name: "Smith", hourly_rate: 55)
+@store2.employees.create(first_name: "Emily", last_name: "Jones", hourly_rate: 45)
