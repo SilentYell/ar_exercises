c6fb0d7584d6ce5e8d8049c21bb506c57a347bb9
Nik Hixson <nikilobe@gmail.com>
2025-03-14 13:05:02 -0700

    Add total and average revenue calculations and count of million-dollar stores

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+total_revenue = Store.sum(:annual_revenue)
+puts "Total Revenue: #{total_revenue}"
+
+average_revenue = Store.average(:annual_revenue)
+puts "Average Annual Revenue: #{average_revenue}"
+
+million_dollar_stores = Store.where("annual_revenue >= ?", 1000000).count
+puts "Number of stores generating $1M or more: #{million_dollar_stores}"
