2a284361965b130eb9ae3b84029ac88743dac473
Nik Hixson <nikilobe@gmail.com>
2025-03-14 12:59:06 -0700

    Add new store records for Surrey, Whistler, and Yaletown

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,6 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+Store.create(name: "Surrey", annual_revenue: 224000, womens_apparel: true)
+Store.create(name: "Whistler", annual_revenue: 1900000, womens_apparel: true)
+Store.create(name: "Yaletown", annual_revenue: 22, womens_apparel: true)
