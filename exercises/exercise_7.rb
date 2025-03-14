ae6287fd2a6ece737909f56a2630a87742f7eff9
Nik Hixson <nikilobe@gmail.com>
2025-03-14 13:04:52 -0700

    Add validations for Store and Employee models and update store name

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,8 +10,21 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+class Store < ActiveRecord::Base
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validate :must_carry_mens_or_womens_apparel
 
-@store1 = Store.find(1)
-@store2 = Store.find(2)
+  def must_carry_mens_or_womens_apparel
+    if !mens_apparel && !womens_apparel
+      errors.add(:base, "Stores must carry at least one of the men's or women's apparel")
+    end
+  end
+end
 
-@store.update(name: "New Burnaby")
+class Employee < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: { only_integer: true, greater_than_or_equal_to: 40, less_than_or_equal_to: 200 }
+  validates :store, presence: true
+end
