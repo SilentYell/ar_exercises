65a696a87c3aa91273d9611814e7e6e2de6b1198
Nik Hixson <nikilobe@gmail.com>
2025-03-14 13:01:08 -0700

    Add functionality to display mens and womens store details based on apparel type and revenue

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -10,3 +10,13 @@ puts "----------"
 Store.create(name: "Surrey", annual_revenue: 224000, womens_apparel: true)
 Store.create(name: "Whistler", annual_revenue: 1900000, womens_apparel: true)
 Store.create(name: "Yaletown", annual_revenue: 22, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+@mens_stores.each do |store|
+  puts "Store name: #{store.name}, Annual revenue: #{store.annual_revenue}"
+end
+
+@womens_stores = Store.where("womens_apparel = ? AND annual_revenue < ?", true, 1000000)
+@womens_stores.each do |store|
+  puts "Store name: #{store.name}, Annual revenue: #{store.annual_revenue}"
+end
